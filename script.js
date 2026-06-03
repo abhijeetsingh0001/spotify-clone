ae68dc47502244b7a3da7718bf33313bfd117210
Abhijeet Singh <mjeet1751@gmail.com>
2026-06-03 22:18:46 +0530

    Implement getSongs function to fetch song links
    Added a function to fetch and process song links from a specified folder.

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Let's write JavaScript");
+
+async function getSongs(folder) {
+    currFolder = folder;
+    let a = await fetch("http://127.0.0.1:5500/song/")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(`/${folder}/`)[1])
+        }
+    }
+  }
