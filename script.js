7f9000e7620af96625f2ddbd61747a61c738f630
Abhijeet Singh <mjeet1751@gmail.com>
2026-06-05 23:09:07 +0530

    Refactor getSongs function and add main function
    Refactor getSongs function to remove folder parameter and update song fetching logic.

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 console.log("Let's write JavaScript");
 
-async function getSongs(folder) {
-    currFolder = folder;
-    let a = await fetch("http://127.0.0.1:5500/song/")
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1])
-        }
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/song/");
+  let response = await a.text();
+
+  console.log(response);
+
+  let div = document.createElement("div");
+  div.innerHTML = response;
+
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
     }
   }
+
+  return songs;
+}
+
+async function main() {
+  // Get the list of songs
+  let songs = await getSongs();
+  console.log(songs);
+
+  let songUL = document.querySelector(".songlist ul");
+
+  for (const song of songs) {
+    songUL.innerHTML += song;
+  }
+
+  // Play the first song
+  let audio = new Audio(songs[0]);
+
+  // audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    let duration = audio.duration;
+    console.log(duration);
+    // duration is in seconds
+  });
+}
+
+main();
