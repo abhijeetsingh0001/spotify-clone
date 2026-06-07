9ccc85abd1b4de8f1740b2678363ede0acc3c868
Abhijeet Singh <mjeet1751@gmail.com>
2026-06-07 10:37:04 +0530

    Refactor song list rendering and play button functionality

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-\console.log("Let's write JavaScript");
+console.log("Let's write JavaScript");
+
+let currentSong = new Audio();
+let play = document.getElementById("play");
 
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
   let response = await a.text();
-
 
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -22,27 +24,53 @@ async function getSongs() {
   return songs;
 }
 
+const playMusic = (track) => {
+  currentSong.src = "/songs/" + track;
+  currentSong.play();
+  play.src = "pause.svg";
+};
+
 async function main() {
   // Get the list of songs
   let songs = await getSongs();
   console.log(songs);
 
-  let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
+  let songUL = document.querySelector(".songlist ul");
 
   for (const song of songs) {
-    songUL.innerHTML += ` <li>
-              <img class="invert" src="music.svg" alt="">
-              <div class="info">
-              <div>${song.replaceAll("%20"," ")}</div>
-              <div>Anant </div></div>
-              <div class="playnow">
-                <span>Play now</span>
-                <img class="invert" src="play.svg" alt="">
-              </div>
-            </li> `;
+    songUL.innerHTML += `
+      <li data-track="${song}">
+        <img class="invert" src="music.svg" alt="">
+        
+        <div class="info">
+          <div>${song.replaceAll("%20", " ")}</div>
+          <div>Anant</div>
+        </div>
+
+        <div class="playnow">
+          <span>Play now</span>
+          <img class="invert" src="play.svg" alt="">
+        </div>
+      </li>`;
   }
 
- 
+  // Attach click event to each song
+  Array.from(songUL.getElementsByTagName("li")).forEach((e) => {
+    e.addEventListener("click", () => {
+      playMusic(e.dataset.track);
+    });
+  });
+
+  // Play/Pause button
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "pause.svg";
+    } else {
+      currentSong.pause();
+      play.src = "play.svg";
+    }
+  });
 }
 
 main();
